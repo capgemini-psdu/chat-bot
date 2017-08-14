f7bce24f53630e738a02b3259ce521746d836047
Phillip Tann <philliptann@Phillips-MacBook-Pro.local>
2017-08-14 13:16:02 +0100

    [TEC-69] Increase acronym data  and json test resource

--- src/test/java/ServiceTest.java
+++ src/test/java/ServiceTest.java
@@ -2,22 +2,21 @@
  * Created by phil on 12/07/17.
  */
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+
 import model.AcronymModel;
 import org.junit.Test;
-import resources.JsonAcronym;
+
 import service.AcronymService;
 
 import java.util.List;
 
 import static org.junit.Assert.*;
-import static org.junit.Assert.assertEquals;
+
 
 public class ServiceTest {
 
     String testValue1="Minimum viable product";
     AcronymService.Model model = new AcronymService.Model();
-    JsonAcronym jsonAcronym;
 
     @Test
     public void test_createPost() throws Exception {
